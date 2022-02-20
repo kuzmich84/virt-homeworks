dc877aefbc83cfde557447a4dafade8dc3bb0c85
Dmitry Kuchukov <kudim@inbox.ru>
2022-02-20 23:11:06 +0300

    ready 05-virt-05-docker-swarm

---
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gj7np30cqm8vkouavc"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g2mc1mgba7si9sl1ss"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8doobjq4tb5s0q778g"
+}
